a1c39bfc58afc2337dfbefe2abc24ffcbeb87895
Kevin <kaoghk@gmail.com>
2021-12-05 16:31:02 -0800

    project updated

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 /**
  * Q2
- * Q3
+ * Q3 Calculate a total leftover free memory (in KB) after applying worst-fit allocation strategy 
+    to place blocks of size 100KB, 500KB, 150KB, 200KB, and 100KB into given initial free 
+    memory blocks of 300KB, 200KB,  600KB and 250KB
  * Q4
+ * 
  */
 
 #include <iostream>
@@ -10,19 +13,50 @@
 
 using namespace std;
 
-void best_fit()
+struct memory_block
 {
+    private:
+        int size;
+        int order;
+    public:
+        memory_block(int size, int order);
+    
+};
+
+
+void best_fit(int blocks, string init_memory_blk_size, string memory_blk_size)
+{
+    // seprate init_memory_blk_size into array
+    int init_memory[5] = {200, 300, 400, 500, 600};
+
+    // best fit algorithm
+    for (int i = 0; i <= blocks; ++i)
+    {
+        memory_block memory(init_memory[i], i);
+    }
+    
+}
+
+void first_fit(int blocks, string init_memory_blk_size, string memory_blk_size)
+{
+    // first fit algorithm
+
+user input 500 300 200 400
+
+200 300 400 500
+
+200 300 400 150
+
+150 200 300 400
+
+
 
 }
 
-void first_fit()
+void worst_fit(int blocks, string init_memory_blk_size, string memory_blk_size)
 {
-
-}
-
-void worst_fit()
-{
-
+    // worse fit algorithm
+    
 }
 
 int main(int argc, char const *argv[])
@@ -37,28 +71,48 @@ int main(int argc, char const *argv[])
     thread t3(worst_fit);
     t3.join();
 
-
     int user_choice;
     int blocks;
-    string initial_memory_blocks;
-    string memory_blocks;
+    string init_memory_blk_size;
+    string memory_blk_size;
+    bool invalid = false;
 
-    cout << "Which method you want to use? " << endl;
-    cout << "1. Best Fit" << endl;
-    cout << "2. Worst Fit" << endl;
-    cout << "3. First Fit" << endl;
-    cout << "Choice: ";
-    cin >> user_choice;
+    // 1 input from user
+    // pass input as object into fit funtion
+    // use threads to run fit functions
+    // print result
 
-    cout << "\nHow free memory blocks? ";
-    cin >> blocks;
+    do 
+    {
+        cout << "Which method you want to use? " << endl;
+        cout << "1. Best Fit" << endl;
+        cout << "2. Worst Fit" << endl;
+        cout << "3. First Fit" << endl;
+        cout << "Choice: ";
+        cin >> user_choice;
+    } while (user_choice != 1 || user_choice != 2 || user_choice != 3);
+    
+    do
+    {
+        cout << "\nHow free memory blocks? ";
+        cin >> blocks;
+    } while (!isdigit(blocks));
+
 
     cout << "\nInitial free memory blocks: ";
-    cin >> initial_memory_blocks;
+    cin >> init_memory_blk_size;
 
     cout << "\nMemory blocks to be placed: " << endl;
-    cin >> memory_blocks;
-    
+    cin >> memory_blk_size;
+
+    if(user_choice == 1)
+        best_fit(blocks, init_memory_blk_size, memory_blk_size);
+    else if(user_choice == 2)
+        worst_fit(blocks, init_memory_blk_size, memory_blk_size);
+    else if(user_choice == 3)
+        first_fit(blocks, init_memory_blk_size, memory_blk_size);
+    else
+        cout << "Invalid Option" << endl;
 
 
     return 0;
--- page.cpp
+++ page.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <pthread.h>
+#include <cmath>
 
-using namespace std
-/**
- * Q9
- */
+using namespace std;
+
+//Question 9
 class Page
 {
     private:
@@ -13,27 +13,46 @@ class Page
         int ram_size;
         int physical_address;
     public:
-
+        //Constructor
+        Page(int p, int bps)
+        {
+            pages = p;
+            byte_page_size = bps;
+        };
+        //setters
+        void set_pages(int p)
+        {
+            pages = p;
+        };
+        void set_byte_page_size(int bps)
+        {
+            byte_page_size = bps;
+        };
+        //Calculating ram_size and physical_address given by the user
+        void calculations()
+        {
+             ram_size = pages * byte_page_size;
+            //Using log base 2 (ram_size) = physical address
+            physical_address = log2(ram_size);
+            cout << "Total RAM size = " << ram_size << endl;
+            cout << "Physical Address = " << physical_address << endl;
+        };
 };
 
 int main(int argc, char const *argv[])
 {
+    int pages, byte_page_size;
+
     cout << "By entering page number and byte page size. Calculate the total RAM size"
          << " and how many bits are needed to each physical address." << endl;
 
-    cout << "Please enter a page number " << endl;
+    cout << "Please enter a page number: " << endl;
     cin >> pages;
 
-    cout << "Please enter a byte page size" << endl;
+    cout << "Please enter a byte page size: " << endl;
     cin >> byte_page_size;
 
-    ram_size = pages * byte_page_size;
-    // log base 2 (ram_size) = physical address
-    physical_address = log2(ram_size);
+    Page user_input(pages, byte_page_size);
+    user_input.calculations();
 
-    cout << "Total RAM size = " << ram_size << endl;
-    cout << "Physical Address = " << physical_address << endl;
-
-
-    
 }
